ed16ebda7b7b902a51876b7347e90773cac70605
lujie <lujie@celes.sg>
2018-11-13 10:43:36 +0800

    update files

--- js/src/Main.js
+++ js/src/Main.js
@@ -183,7 +183,7 @@ function handleMove(e){
 	if (e.targetTouches.length == 1) {
 		var touch = e.targetTouches[0];
 
-		buildBladeParticle(touch.pageX, touch.pageY);
+		buildBladeParticle(touch.screenX, touch.screenY);
 	  }
 }
 

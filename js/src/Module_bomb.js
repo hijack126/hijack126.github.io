a0929612fe1180bcb056f4ee8b2c0e8e3d396d77
lujie <lujie@celes.sg>
2018-11-28 17:25:39 +0800

    Add special checkout support

--- js/src/Module_bomb.js
+++ js/src/Module_bomb.js
@@ -58,6 +58,7 @@
 				+ assetsManager.bomb.height, Infinity, assetsManager.bomb,
 				assetsManager.shadow, middleContext);
 		p.bottomY = gameHeight + assetsManager.bomb.height;
+		p.scale = 1.8;
 	};
 	// cut bomb
 	cutBomb = function(target) {

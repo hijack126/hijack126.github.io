7c2a6265e49031fcd06ec6ba44e79533f9520840
lujie <lujie@celes.sg>
2018-12-05 10:26:15 +0800

    update files

--- js/src/Module_ui.js
+++ js/src/Module_ui.js
@@ -12,7 +12,7 @@
 		 ui_gameTitle = particleSystem.createParticle(SPP.SpriteImage);
 		 ui_gameTitle.regX= ui_gameTitle.regY=0;
 		 ui_gameTitle.init(0,-assetsManager.gametitle.height,Infinity,assetsManager.gametitle,topContext);
-		 TweenLite.to(ui_gameTitle.position,100,{y:0});
+		 TweenLite.to(ui_gameTitle.position,30,{y:0});
 		 
 		ui_newGame = particleSystem.createParticle(SPP.SpriteImage);
 		ui_newGame.init(gameWidth*0.618,gameHeight*0.618,Infinity,assetsManager.newgame,topContext);

1169f79fd0d092abc45a80e916ea73cacee65e94
lujie <lujie@celes.sg>
2019-01-07 16:49:25 +0800

    update

--- js/src/Module_ui.js
+++ js/src/Module_ui.js
@@ -94,7 +94,7 @@
 
 	    ui_gameOverScore = particleSystem.createParticle(SPP.SpriteImage);
 		ui_gameOverScore.regX=0;
-		ui_gameOverScore.init(0,-assetsManager.gamescorebg.heigh*2,Infinity,assetsManager.gamescorebg,topContext);
+		ui_gameOverScore.init(100,100,Infinity,assetsManager.gamescorebg,topContext);
 		ui_gameOverScore.scale = 2;
 		 
 	};

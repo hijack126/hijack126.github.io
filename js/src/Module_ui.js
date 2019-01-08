d4dd539f437513351779cd74016fcdc5131e3c37
lujie <lujie@celes.sg>
2019-01-08 14:27:57 +0800

    update

--- js/src/Module_ui.js
+++ js/src/Module_ui.js
@@ -155,6 +155,7 @@
 
 	showGameoverUI=function()
 	{
+		ui_gameOver.life=0;
 		ui_gameOver = particleSystem.createParticle(SPP.SpriteImage);
 		ui_gameOver.init(gameWidth*0.5,gameHeight*0.5,Infinity,assetsManager.gameover,topContext);
 		ui_gameOver.scale=0;

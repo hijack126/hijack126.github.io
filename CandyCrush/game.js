e37c2e737d23fb487ff1139265f64258a3bdbb7b
lujie <lujie@celes.sg>
2019-02-19 10:03:35 +0800

    update

--- CandyCrush/game.js
+++ CandyCrush/game.js
@@ -71,6 +71,23 @@ Main.prototype={
         me.tileContainer = new TileContainer(me.game, 0 , me.offsety);
         me.tileContainer.addChild(me.tiles);
 
+        var defaultConfig= {
+            width: 250,
+            height: 40,
+            x: 0,
+            y: 0,
+            bg: {
+              color: '#651828'
+            },
+            bar: {
+              color: '#FEFF03'
+            },
+            animationDuration: 200,
+            flipped: false
+          };
+
+        me.myScoreBar = new ScoreBar(this.game, defaultConfig);
+
         var seed=Date.now();
         me.random=new Phaser.RandomDataGenerator([seed]);
 
@@ -475,6 +492,91 @@ Main.prototype={
     }
 };
 
+var ScoreBar = function(game, providedConfig) {
+    this.game = game;
+   
+    this.setupConfiguration(providedConfig);
+    this.setPosition(this.config.x, this.config.y);
+    this.drawBackground();
+    this.drawScoreBar();
+};
+ScoreBar.prototype.constructor = ScoreBar;
+module.exports = ScoreBar; 
 
+ScoreBar.prototype.setupConfiguration = function (providedConfig) {
+    this.config = this.mergeWithDefaultConfiguration(providedConfig);
+};
 
+ScoreBar.prototype.drawBackground = function() {
+ 
+    var bmd = this.game.add.bitmapData(this.config.width, this.config.height);
+    bmd.ctx.fillStyle = this.config.bg.color;
+    bmd.ctx.beginPath();
+    bmd.ctx.rect(0, 0, this.config.width, this.config.height);
+    bmd.ctx.fill();
+   
+    this.bgSprite = this.game.add.sprite(this.x, this.y, bmd);
+    this.bgSprite.anchor.set(0.5);
+};
 
+ScoreBar.prototype.drawScoreBar = function() {
+    var bmd = this.game.add.bitmapData(this.config.width, this.config.height);
+    bmd.ctx.fillStyle = this.config.bar.color;
+    bmd.ctx.beginPath();
+    bmd.ctx.rect(0, 0, this.config.width, this.config.height);
+    bmd.ctx.fill();
+   
+    this.barSprite = this.game.add.sprite(this.x - this.bgSprite.width/2, this.y, bmd);
+    this.barSprite.anchor.y = 0.5;
+};
+
+ScoreBar.prototype.setPosition = function (x, y) {
+    this.x = x;
+    this.y = y;
+   
+    if(this.bgSprite !== undefined &amp;amp;amp;amp; this.barSprite !== undefined){
+      this.bgSprite.position.x = x;
+      this.bgSprite.position.y = y;
+   
+      this.barSprite.position.x = x - this.config.width/2;
+      this.barSprite.position.y = y;
+    }
+};
+
+ScoreBar.prototype.mergeWithDefaultConfiguration = function(newConfig) {
+    var defaultConfig= {
+      width: 250,
+      height: 40,
+      x: 0,
+      y: 0,
+      bg: {
+        color: '#651828'
+      },
+      bar: {
+        color: '#FEFF03'
+      }
+    };
+   
+    return mergeObjetcs(defaultConfig, newConfig);
+};
+   
+function mergeObjetcs(targetObj, newObj) {
+    for (var p in newObj) {
+      try {
+        targetObj[p] = newObj[p].constructor==Object ? mergeObjetcs(targetObj[p], newObj[p]) : newObj[p];
+      } catch(e) {
+        targetObj[p] = newObj[p];
+      }
+    }
+    return targetObj;
+}
+
+ScoreBar.prototype.setPercent = function(newValue){
+    if(newValue &amp;amp;amp;lt; 0) newValue = 0;   if(newValue &amp;amp;amp;gt; 100) newValue = 100;
+    var newWidth =  (newValue * this.config.width) / 100;
+    this.setWidth(newWidth);
+};
+   
+ScoreBar.prototype.setWidth = function(newWidth){
+   this.game.add.tween(this.barSprite).to( { width: newWidth }, this.config.animationDuration, Phaser.Easing.Linear.None, true);    
+};

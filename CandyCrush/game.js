4af45efab5054912c9de6760a8cc9a5aea02ac2d
lujie <lujie@celes.sg>
2019-02-20 11:48:44 +0800

    update

--- CandyCrush/game.js
+++ CandyCrush/game.js
@@ -360,7 +360,7 @@ Main.prototype={
         this.backpackhud.inputEnabled = true;
         var backpackhudBadge = game.add.graphics(0, 0);
         backpackhudBadge.beginFill(0xFF0000, 1);
-        backpackhudBadge.drawCircle(150, 650, 20);
+        backpackhudBadge.drawCircle(180, 680, 20);
         this. backpackLabel=this.game.add.text(150,650,"0",{font:badgeFont,fill:"#fff"});
         this.backpackLabel.anchor.setTo(0.5);
         this.backpackLabel.align='center';

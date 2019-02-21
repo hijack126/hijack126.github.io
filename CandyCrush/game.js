39bd92a18923ee7ec121db6a62e7871a7d8256c2
lujie <lujie@celes.sg>
2019-02-21 17:38:16 +0800

    update

--- CandyCrush/game.js
+++ CandyCrush/game.js
@@ -73,7 +73,7 @@ Main.prototype={
                      [null,null,null,null,null,null],
                      [null,null,null,null,null,null]]
 
-        var w1 = game.width *0.95;
+        var w1 = game.width *0.96;
         me.tileWidth=w1/6;
         me.tileHeight=me.tileWidth;
 

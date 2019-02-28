ef8c59048dab217f572d4e4561b0bcdbe153c340
lujie <lujie@celes.sg>
2019-02-28 17:59:16 +0800

    update

--- CandyCrush/game.js
+++ CandyCrush/game.js
@@ -38,7 +38,7 @@ var TileContainer = function(game, x, y, w, h){
     width = w;
     height = h;
     Phaser.Sprite.call(this, game, x, y, "");
-    var tile = this.game.add.image(0, 0,'tile');
+    var tile = this.game.add.image(100, 0,'tile');
     tile.anchor.setTo(0.5);
     this.anchor.setTo(0.5);
     game.add.existing(this); 

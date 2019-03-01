36d103bece56329dcf85e159aae55025084c8746
lujie <lujie@celes.sg>
2019-03-01 14:56:16 +0800

    update

--- CandyCrush/game.js
+++ CandyCrush/game.js
@@ -142,6 +142,8 @@ Main.prototype={
         var me=this;
         var tileToAdd=me.tileTypes[me.random.integerInRange(0,me.tileTypes.length-1)];
         var tile=me.tiles.create((x*me.tileWidth)+me.tileWidth/2, 0, tileToAdd);
+        tile.width = me.tileWidth;
+        tile.height = me.tileWidth;
         me.game.add.tween(tile).to({y:y*me.tileHeight+me.tileHeight/2},500,Phaser.Easing.Linear.In,true);
         tile.anchor.setTo(0.5);
         tile.inputEnabled=true;

fdc935bb8402fe4d5820c8647be01a2acb884f12
lujie <lujie@celes.sg>
2019-03-04 10:14:46 +0800

    update

--- CandyCrush/game.js
+++ CandyCrush/game.js
@@ -350,15 +350,15 @@ Main.prototype={
     },
     createScore:function(){
         var me=this;
-        me.game.add.image(20, 120,'scoreBackground').scale.setTo(0.5);
+        me.game.add.image(10, 110,'scoreBackground').scale.setTo(0.5);
         var scoreFont="20px Arial";
-        me.scoreLabel=me.game.add.text(60,110,"0",{font:scoreFont,fill:"#000"});
+        me.scoreLabel=me.game.add.text(80,120,"0",{font:scoreFont,fill:"#000"});
         me.scoreLabel.anchor.setTo(0.5,0);
         me.scoreLabel.strokeThickness = 2;
         me.scoreLabel.align='center';
 
         var defaultConfig= {
-            width: 200,
+            width: 180,
             height: 30,
             x: game.width - 150,
             y: 140,

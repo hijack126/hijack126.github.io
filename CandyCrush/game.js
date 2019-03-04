60a1353a7d73cbafb1d1b9c209c40e1404948648
lujie <lujie@celes.sg>
2019-03-04 10:38:11 +0800

    update

--- CandyCrush/game.js
+++ CandyCrush/game.js
@@ -642,7 +642,7 @@ ScoreBar.prototype.setupConfiguration = function (providedConfig) {
 
 ScoreBar.prototype.drawBackground = function() {
  
-    this.game.add.image(this.x - 50, this.y-30,'starbg').scale.setTo(0.7);
+    this.game.add.image(this.x - 70, this.y-20,'starbg').scale.setTo(0.7);
 
     var bmd = this.game.add.bitmapData(this.config.width, this.config.height);
     bmd.ctx.fillStyle = this.config.bg.color;

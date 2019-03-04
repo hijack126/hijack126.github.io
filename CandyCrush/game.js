640a5810bc6b28246e7243cf3f22f3b12525bba9
lujie <lujie@celes.sg>
2019-03-04 14:25:59 +0800

    update

--- CandyCrush/game.js
+++ CandyCrush/game.js
@@ -105,7 +105,7 @@ Main.prototype={
         me.popup = game.add.sprite(game.world.centerX, game.world.centerY, 'powerupHintBackground');
         me.popup.anchor.set(0.5);
         me.popup.scale.setTo(0.0);
-        me.popupClose = game.add.sprite(game.world.centerX + 130, game.world.centerY - 200, 'powerupHintClose');
+        me.popupClose = game.add.sprite(game.world.centerX + 140, game.world.centerY - 200, 'powerupHintClose');
         me.popupClose.anchor.set(0.5);
         me.popupClose.scale.setTo(0.0);
 

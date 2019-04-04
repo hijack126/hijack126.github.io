692ad3a2952b7c3a1350ca883426f03cf16d8ef9
lujie <lujie@celes.sg>
2019-04-04 16:52:07 +0800

    update

--- CandyCrush/game.js
+++ CandyCrush/game.js
@@ -52,7 +52,7 @@ var TileContainer = function(game, x, y, w, h){
     Phaser.Sprite.call(this, game, x, y, "");
     var tile = null;
     if(window.innerWidth <= window.innerHeight){
-        tile = this.game.add.image(window.innerWidth/2, 350,'tile');
+        tile = this.game.add.image(window.innerWidth/2, 340,'tile');
     }else{
         tile = this.game.add.image(200, 400,'tile');
     }
@@ -103,7 +103,7 @@ Main.prototype={
         me.tileHeight =  me.tileWidth;
 
         if(window.innerWidth <= window.innerHeight){
-            me.tileContainer = new TileContainer(me.game, 20 , me.offsety - 50, w1, w1);
+            me.tileContainer = new TileContainer(me.game, 20 , me.offsety - 60, w1, w1);
         }else{
             me.tileContainer = new TileContainer(me.game, 20 , me.offsety, w1, w1);
         }

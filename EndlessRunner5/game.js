3cd9609bad115af96450740bf653b33200352df7
lujie <lujie@celes.sg>
2020-11-03 11:15:30 +0800

    update

--- EndlessRunner5/game.js
+++ EndlessRunner5/game.js
@@ -70,7 +70,7 @@ window.onload = function() {
         physics: {
             default: "arcade",
             arcade: {
-                debug: true
+                debug: false
             }
         }
     }
